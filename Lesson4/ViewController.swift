6e893713106448f4f7d5ed8622665644d5f0d71f
fangfang <wff820901@hotmail.com>
2017-02-09 19:19:02 +0800

    Initial commit

--- Lesson4/ViewController.swift
+++ Lesson4/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let myLabel = UILabel(frame: CGRect(origin: .zero, size: CGSize(width: 150, height: 30)))
+        
+        myLabel.text = "added"
+        view.addSubview(myLabel)
+        
     }
 
     override func didReceiveMemoryWarning() {
